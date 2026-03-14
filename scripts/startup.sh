db5dacaa778896e3c68d3a064547c7cf90c9ed1f
gwalczak2005 <gianluca.walczak@student.htw-berlin.de>
2026-03-14 12:45:45 +0100

    init-all zu startup umbenannt

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -5,36 +5,32 @@ PROJECT_ROOT=$HOME/testbackend
 INFRA_PATH=$PROJECT_ROOT/infrastructure
 APP_WALLET=$PROJECT_ROOT/application/wallet/org1-admin
 
-echo "🧼 STARTE RADIKALEN REINIGUNGSPROZESS..."
-
-#Zusatz: sensor_data.db entfernen
-echo "🧹 Lösche sensor_data.db"
-rm -f ./application/sensor_data.db
+echo "STARTE REINIGUNGSPROZESS"
 
 # 1. Infrastruktur stoppen
 cd $INFRA_PATH
 ./network.sh down
 
 # 2. Docker komplett aufräumen
-echo "🗑️  Lösche verwaiste Docker-Container und Volumes..."
+echo "Lösche verwaiste Docker-Container und Volumes..."
 docker rm -f $(docker ps -aq) 2>/dev/null
 docker volume prune -f
 docker network prune -f
 
 # 3. Wallet zurücksetzen
-echo "🔐 Lösche altes Wallet..."
+echo "Lösche altes Wallet..."
 rm -rf $APP_WALLET/*
 
 # 4. Netzwerk neu starten
-echo "🚀 Starte Netzwerk und erstelle Channel..."
+echo "Starte Netzwerk und erstelle Channel..."
 ./network.sh up createChannel -c mychannel -ca
 
 # 5. Chaincode deployen (Lokaler Pfad)
-echo "📦 Installiere Smart Contract..."
+echo "Installiere Smart Contract..."
 ./network.sh deployCC -ccn basic -ccp ../chaincode -ccl javascript
 
 # 6. Zertifikate für das Backend synchronisieren
-echo "🔑 Synchronisiere neue Zertifikate..."
+echo "Synchronisiere neue Zertifikate..."
 mkdir -p $APP_WALLET/msp/signcerts
 mkdir -p $APP_WALLET/msp/keystore
 mkdir -p $APP_WALLET/tls
@@ -49,8 +45,8 @@ CREATE TABLE IF NOT EXISTS api_users (id INTEGER PRIMARY KEY AUTOINCREMENT, api_
 INSERT INTO api_users (api_key, role, owner) VALUES ('MASTER_ADMIN_2026', 'ADMIN', 'Großunternehmen');
 EOF
 
-echo "✅ Datenbank-Reset durchgeführt und Standard-API-Keys hinterlegt."
-echo "⏳ Kurze Pause für den Chaincode-Container (15s)..."
+echo "Datenbank-Reset durchgeführt, Standard-API-Keys hinterlegt."
+echo "Chaincode-Container-Pause"
 sleep 15
 
 echo "--------------------------------------------------"
